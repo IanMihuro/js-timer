596a4582af7dbdb92e9e9395ef3c27d837c0c109
Ian Mihuro <ianmihuro@Ians-MacBook-Pro.local>
2020-03-12 20:43:04 +0300

    timer class added

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-console.log("Hello there!");
+class Timer {
+  constructor(durationInput, startBtn, pauseBtn) {
+    this.durationInput = durationInput;
+    this.startBtn = startBtn;
+    this.pauseBtn = pauseBtn;
+
+    this.startBtn.addEventListener("click", this.start);
+    this.pauseBtn.addEventListener("click", this.pause);
+  }
+
+  start = () => {
+    this.tick();
+    this.interval = setInterval(this.tick, 1000);
+  };
+
+  tick = () => {
+    if (this.timeRemaining <= 0) {
+      this.pause();
+    } else {
+      this.timeRemaining = this.timeRemaining - 1;
+    }
+  };
+
+  get timeRemaining() {
+    return parseFloat(this.durationInput.value);
+  }
+
+  set timeRemaining(time) {
+    return (this.durationInput.value = time);
+  }
+
+  pause = () => clearInterval(this.interval);
+
+  durationChange = () => {};
+}
+
+const durationInput = document.querySelector("#duration");
+const startBtn = document.querySelector("#start");
+const pauseBtn = document.querySelector("#pause");
+
+const timer = new Timer(durationInput, startBtn, pauseBtn);
